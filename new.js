3e46d3fc8528cace6864dfe48b8769b3e417dec6
mohitsh7979 <mohitsh7979@gmail.com>
2024-04-15 14:34:20 +0530

    upodat

---
+++ new.js
@@ -0,0 +1,16 @@
+let a = {
+  name: "mohit",
+  age: 25
+};
+
+// let b = {
+//   name: "ankit",
+//   age: 50,
+// };
+
+// let [x,y,...z] = [10,20,b,a];
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
